c4e0095f8de3a74e068ec016cf5597669e8082f9
Konstantin108 <Kpstya1@yandex.ru>
2022-09-10 15:59:07 +0300

    some little fixes more2

--- resources/views/layouts/prod.blade.php
+++ resources/views/layouts/prod.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Marketplace</title>
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,400i,700,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
     <script src="https://kit.fontawesome.com/4bd251a57a.js" crossorigin="anonymous"></script>
